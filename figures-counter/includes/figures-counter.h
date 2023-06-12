487aeec240d54ff431d2c963dd9a64509cf3cdf0
Ivan Mollov <ivan.xero.mollov@gmail.com>
2023-06-12 05:01:44 +0300

    Improve error handling
    Introduced and tested error hierarchy

--- figures-counter/includes/figures-counter.h
+++ figures-counter/includes/figures-counter.h
@@ -12,6 +12,95 @@
 
 namespace figures_counter {
 
+class figures_counter_exception : public std::invalid_argument {
+public:
+	figures_counter_exception(const std::string& what) :
+		invalid_argument(std::format("figures counter error: {}", what)) {
+	}
+};
+
+class bad_input : public figures_counter_exception {
+public:
+	bad_input(const std::string& what) :
+		figures_counter_exception(std::format("bad input: {}", what)) {
+	}
+};
+
+class row_length_error : public bad_input {
+public:
+	row_length_error(unsigned row, unsigned length, unsigned expected_length) :
+		bad_input(std::format("row {} with length {}, expected length {}", row, length, expected_length)),
+		_row(row),
+		_length(length),
+		_expected_length(expected_length) {
+	}
+
+	unsigned row() const noexcept {
+		return _row;
+	}
+
+	unsigned length() const noexcept {
+		return _length;
+	}
+
+	unsigned expected_length() const noexcept {
+		return _expected_length;
+	}
+
+private:
+	unsigned _row;
+	unsigned _length;
+	unsigned _expected_length;
+};
+
+class row_count_error : public bad_input {
+public:
+	row_count_error(unsigned count, unsigned expected_count) :
+		bad_input(std::format("row count is {}, expected count {}", count, expected_count)),
+		_count(count),
+		_expected_count(expected_count) {
+	}
+
+	unsigned count() const noexcept {
+		return _count;
+	}
+
+	unsigned expected_count() const noexcept {
+		return _expected_count;
+	}
+
+private:
+	unsigned _count;
+	unsigned _expected_count;
+};
+
+class unrecognized_symbol : public bad_input {
+public:
+	unrecognized_symbol(unsigned row, unsigned col, char symbol) :
+		bad_input(std::format("received unrecognized char \"{}\" at [row = {}, col = {}]", symbol, row, col)),
+		_row(row),
+		_col(col),
+		_symbol(symbol) {
+	}
+
+	unsigned row() const noexcept {
+		return _row;
+	}
+
+	unsigned col() const noexcept {
+		return _col;
+	}
+
+	char symbol() const noexcept {
+		return _symbol;
+	}
+
+private:
+	unsigned _row;
+	unsigned _col;
+	char _symbol;
+};
+
 /**
  * @brief The accepted input values for a cell of the input table
  */
@@ -47,6 +136,16 @@ constexpr unsigned EMPTY_FIGURE_ID = UINT_MAX;
  */
 
 int count_figures(int rows, int cols, std::istream& data) {
+	if (rows <= 0) {
+		throw bad_input("rows must be positive");
+	}
+	if (cols <= 0) {
+		throw bad_input("cols must be positive");
+	}
+	if (!data) {
+		throw bad_input("data stream can not be read");
+	}
+
 	std::string buffer;
 	buffer.reserve(cols);
 
@@ -62,13 +161,18 @@ int count_figures(int rows, int cols, std::istream& data) {
 		current_row.clear();
 
 		std::getline(data, buffer);
-		if (buffer.size() != cols) {
-			throw std::invalid_argument("shorter than expected buffer");
-		}
 		return data;
 	};
 
-	for (size_t row = 0; row < rows && load_next_row(); ++row) {
+	for (size_t row = 0; row < rows; ++row) {
+		load_next_row();
+		if (!data) {
+			throw row_count_error(row, rows);
+		}
+		if (buffer.size() != cols) {
+			throw row_length_error(row, buffer.size() - 1, cols);
+		}
+
 		// Cache the left cell in to skip check whether the current cell is 0
 		unsigned left_cell_figure = EMPTY_FIGURE_ID;
 
@@ -95,19 +199,16 @@ int count_figures(int rows, int cols, std::istream& data) {
 					current_row.push_back(left_cell_figure = sorted.first);
 				}
 			} else [[unlikely]] {
-				throw std::invalid_argument("unrecognized symbol");
+				throw unrecognized_symbol(row, col, cell);
 			}
-		}
-
-		if (current_row.size() != prev_row.size()) {
-			throw std::logic_error(
-				std::format("current_row.size() is {}, prev_row.size() is {}", current_row.size(), prev_row.size()));
 		}
 	}
 
 	std::sort(std::begin(figure_ids), std::end(figure_ids));
-	const int figures_count =
-		static_cast<int>(std::unique(std::begin(figure_ids), std::end(figure_ids)) - std::begin(figure_ids));
+	const auto unique_ids_end = std::unique(std::begin(figure_ids), std::end(figure_ids));
+
+	const int figures_count = static_cast<int>(unique_ids_end - std::begin(figure_ids));
+	assert(figures_count > 0);
 	return figures_count;
 }
 
--- tests/main.cpp
+++ tests/main.cpp
@@ -8,13 +8,6 @@
 
 BOOST_AUTO_TEST_SUITE(CountFigures)
 
-BOOST_AUTO_TEST_CASE(EmptyInput) {
-	const std::string m = "\n";
-	std::stringstream m_stream(m);
-
-	BOOST_REQUIRE_EQUAL(figures_counter::count_figures(0, 0, m_stream), 0);
-}
-
 BOOST_AUTO_TEST_CASE(SingleCellFigure) {
 	const std::string m = "      *\n";
 	std::stringstream m_stream(m);
@@ -66,10 +59,10 @@ BOOST_AUTO_TEST_CASE(BigFigureSparsed) {
 		std::string m_row;
 		if (row % 2 == 0) {
 			for (auto col = 0; col < cols; ++col) {
-				m_row += static_cast<char>(col % 2 == 0 ? figures_counter::Cell::Figure : figures_counter::Cell::Background);
+				m_row +=
+					static_cast<char>(col % 2 == 0 ? figures_counter::Cell::Figure : figures_counter::Cell::Background);
 			}
-		}
-		else {
+		} else {
 			m_row = std::string(cols, static_cast<char>(figures_counter::Cell::Background));
 		}
 		m_row += "\n";
@@ -79,6 +72,63 @@ BOOST_AUTO_TEST_CASE(BigFigureSparsed) {
 	std::stringstream m_stream(m);
 
 	BOOST_REQUIRE_EQUAL(figures_counter::count_figures(rows, cols, m_stream), figures_count);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(BadInput)
+
+BOOST_AUTO_TEST_CASE(EmptyInput) {
+	const std::string m = "\n";
+	std::stringstream m_stream(m);
+
+	BOOST_REQUIRE_THROW(figures_counter::count_figures(0, 0, m_stream), figures_counter::bad_input);
+}
+
+BOOST_AUTO_TEST_CASE(InvalidRowCount) {
+	const std::string m = "*\n";
+	std::stringstream m_stream(m);
+
+	BOOST_REQUIRE_THROW(figures_counter::count_figures(-1, 1, m_stream), figures_counter::bad_input);
+}
+
+BOOST_AUTO_TEST_CASE(InvalidColCount) {
+	const std::string m = "*\n";
+	std::stringstream m_stream(m);
+
+	BOOST_REQUIRE_THROW(figures_counter::count_figures(1, -1, m_stream), figures_counter::bad_input);
+}
+
+BOOST_AUTO_TEST_CASE(InvalidData) {
+	const std::string m = "*\n";
+	std::stringstream m_stream(m);
+	std::string m_stream_content;
+	// Put the stream to bad state by reading all its contents
+	std::getline(m_stream, m_stream_content);
+
+	BOOST_REQUIRE_THROW(figures_counter::count_figures(1, 1, m_stream), figures_counter::bad_input);
+}
+
+BOOST_AUTO_TEST_CASE(BadRowLength) {
+	const std::string m = " * \n";
+	std::stringstream m_stream(m);
+
+	BOOST_REQUIRE_THROW(figures_counter::count_figures(1, 2, m_stream), figures_counter::row_length_error);
+}
+
+BOOST_AUTO_TEST_CASE(BadRowCount) {
+	const std::string m = " * \n"
+						  " * \n";
+	std::stringstream m_stream(m);
+
+	BOOST_REQUIRE_THROW(figures_counter::count_figures(3, 3, m_stream), figures_counter::row_count_error);
+}
+
+BOOST_AUTO_TEST_CASE(UnrecognizedSymbol) {
+	const std::string m = " 8 \n";
+	std::stringstream m_stream(m);
+
+	BOOST_REQUIRE_THROW(figures_counter::count_figures(1, 3, m_stream), figures_counter::unrecognized_symbol);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
