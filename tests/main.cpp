cc242834791c811b4d1994564d163500b78d79fa
Ivan Mollov <ivan.xero.mollov@gmail.com>
2023-06-12 03:30:16 +0300

    Add more positive tests
    Adds more tests that test figures counting.

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,21 +2,83 @@
 
 #include <figures-counter.h>
 
-#include <boost/test/auto_unit_test.hpp>
 #include <boost/test/unit_test.hpp>
 
 #include <sstream>
 
-BOOST_AUTO_TEST_CASE(MatrixCreate) {
-	const std::string m = 
-		"*     ***     \n"
-		"     ** **    \n"
-		"    **   **   \n"
-		"    *     *   \n"
-		"    **   **   \n"
-		"     ** **    \n"
-		"      ***     \n"; 
+BOOST_AUTO_TEST_SUITE(CountFigures)
+
+BOOST_AUTO_TEST_CASE(EmptyInput) {
+	const std::string m = "\n";
 	std::stringstream m_stream(m);
-	
-	BOOST_REQUIRE_EQUAL(figures_counter::count_figures(7, 14, m_stream), 2);
+
+	BOOST_REQUIRE_EQUAL(figures_counter::count_figures(0, 0, m_stream), 0);
 }
+
+BOOST_AUTO_TEST_CASE(SingleCellFigure) {
+	const std::string m = "      *\n";
+	std::stringstream m_stream(m);
+
+	BOOST_REQUIRE_EQUAL(figures_counter::count_figures(1, 7, m_stream), 1);
+}
+
+BOOST_AUTO_TEST_CASE(SimpleFigure) {
+	const std::string m = "      *\n"
+						  "      *\n";
+	std::stringstream m_stream(m);
+
+	BOOST_REQUIRE_EQUAL(figures_counter::count_figures(2, 7, m_stream), 1);
+}
+
+BOOST_AUTO_TEST_CASE(CircularFigure) {
+	const std::string m = "      ***     \n"
+						  "     ** **    \n"
+						  "    **   **   \n"
+						  "    *     *   \n"
+						  "    **   **   \n"
+						  "     ** **    \n"
+						  "      ***     \n";
+	std::stringstream m_stream(m);
+
+	BOOST_REQUIRE_EQUAL(figures_counter::count_figures(7, 14, m_stream), 1);
+}
+
+BOOST_AUTO_TEST_CASE(CircularFigureWithRetractions) {
+	const std::string m = "*     ***     \n"
+						  "     ** **    \n"
+						  "    **   **   \n"
+						  " ****     *   \n"
+						  " *  **   **   \n"
+						  "     ** **    \n"
+						  "  *   ***     \n";
+	std::stringstream m_stream(m);
+
+	BOOST_REQUIRE_EQUAL(figures_counter::count_figures(7, 14, m_stream), 3);
+}
+
+BOOST_AUTO_TEST_CASE(BigFigureSparsed) {
+	const unsigned rows = 1000;
+	const unsigned cols = 1000;
+	const unsigned figures_count = rows * cols / 4;
+
+	std::string m;
+	for (auto row = 0; row < rows; ++row) {
+		std::string m_row;
+		if (row % 2 == 0) {
+			for (auto col = 0; col < cols; ++col) {
+				m_row += static_cast<char>(col % 2 == 0 ? figures_counter::Cell::Figure : figures_counter::Cell::Background);
+			}
+		}
+		else {
+			m_row = std::string(cols, static_cast<char>(figures_counter::Cell::Background));
+		}
+		m_row += "\n";
+		m += m_row;
+	}
+
+	std::stringstream m_stream(m);
+
+	BOOST_REQUIRE_EQUAL(figures_counter::count_figures(rows, cols, m_stream), figures_count);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
