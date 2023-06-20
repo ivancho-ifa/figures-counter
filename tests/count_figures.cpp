0fee4fdbfbd9b9f610d748d804e427a618a9e1c9
Ivan Mollov <ivan.xero.mollov@gmail.com>
2023-06-20 10:35:56 +0300

    Fix test image

--- tests/count_figures.cpp
+++ tests/count_figures.cpp
@@ -75,8 +75,7 @@ BOOST_AUTO_TEST_CASE(BigFigureSparsed) {
 BOOST_AUTO_TEST_SUITE(BmpFiguresCounter)
 
 BOOST_AUTO_TEST_CASE(CircularFigureWithRetractions) {
-
-	BOOST_REQUIRE_EQUAL(figures_counter::count_figures("1080p.bmp"), 5);
+	BOOST_REQUIRE_EQUAL(figures_counter::count_figures("1080p.bmp"), 6);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
